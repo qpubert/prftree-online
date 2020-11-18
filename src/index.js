0d975bd1004397f7a6269c7734820f363234559f
Quentin PUBERT <quentin.pubert@imt-atlantique.net>
2020-11-18 15:33:31 +0100

    Add some Tex/MathJax voilà

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Split from 'react-split';
+import { MathComponent } from 'mathjax-react';
 import { v4 as uuidv4 } from 'uuid';
 import './index.css';
 
@@ -45,6 +46,9 @@ class LiveEditor extends React.Component {
         parent: null,
         id: uuidv4(),
         assumptions: [],
+        conclusionFocused: false,
+        labelFocused: false,
+        ruleNameFocused: false,
         label: '',
         ruleName: '',
         conclusion: ''
@@ -53,78 +57,121 @@ class LiveEditor extends React.Component {
   }
 
   renderNode(node) {
-    const label = (
-      <input
-        type='text'
-        placeholder='.'
-        defaultValue={node.label}
-        style={{ width: `${Math.max(1, node.label.length)}ch` }}
-        onInput={(event) => {
-          let rootNodeCopy = { ...this.state.rootNode };
-
-          if (node.id === this.state.rootNode.id) {
-            rootNodeCopy.label = event.target.value;
-          } else {
-            node.label = event.target.value;
-          }
-
-          this.setState({
-            rootNode: rootNodeCopy
-          });
-        }}
+    const label = (!node.labelFocused) ? (
+      <MathComponent
+        display={false}
+        tex={`\\small{\\text{${node.label.length === 0 ? '.' : node.label}}}`}
       />
-    );
+    ) : (
+        <input
+          type='text'
+          placeholder='.'
+          defaultValue={node.label}
+          style={{ width: `${Math.max(1, node.label.length)}ch` }}
+          onInput={(event) => {
+            let rootNodeCopy = { ...this.state.rootNode };
 
-    const ruleName = (
-      <input
-        type='text'
-        placeholder='.'
-        defaultValue={node.ruleName}
-        style={{ width: `${Math.max(1, node.ruleName.length)}ch` }}
-        onInput={(event) => {
-          let rootNodeCopy = { ...this.state.rootNode };
+            if (node.id === this.state.rootNode.id) {
+              rootNodeCopy.label = event.target.value;
+            } else {
+              node.label = event.target.value;
+            }
 
-          if (node.id === this.state.rootNode.id) {
-            rootNodeCopy.ruleName = event.target.value;
-          } else {
-            node.ruleName = event.target.value;
-          }
+            this.setState({
+              rootNode: rootNodeCopy
+            });
+          }}
+        />
+      );
 
-          this.setState({
-            rootNode: rootNodeCopy
-          });
-        }}
+    const ruleName = (!node.ruleNameFocused) ? (
+      <MathComponent
+        display={false}
+        tex={`\\small{\\text{${node.ruleName.length === 0 ? '.' : node.ruleName}}}`}
       />
-    );
+    ) : (
+        <input
+          type='text'
+          placeholder='.'
+          defaultValue={node.ruleName}
+          style={{ width: `${Math.max(1, node.ruleName.length)}ch` }}
+          onInput={(event) => {
+            let rootNodeCopy = { ...this.state.rootNode };
 
-    const conclusion = (
-      <input
-        type='text'
-        placeholder='...'
-        defaultValue={node.conclusion}
-        style={{ width: `${Math.max(3, node.conclusion.length)}ch` }}
-        onInput={(event) => {
-          let rootNodeCopy = { ...this.state.rootNode };
+            if (node.id === this.state.rootNode.id) {
+              rootNodeCopy.ruleName = event.target.value;
+            } else {
+              node.ruleName = event.target.value;
+            }
 
-          if (node.id === this.state.rootNode.id) {
-            rootNodeCopy.conclusion = event.target.value;
-          } else {
-            node.conclusion = event.target.value;
-          }
+            this.setState({
+              rootNode: rootNodeCopy
+            });
+          }}
+        />
+      );
 
-          this.setState({
-            rootNode: rootNodeCopy
-          });
-        }}
+    const conclusion = (!node.conclusionFocused) ? (
+      <MathComponent
+        display={false}
+        tex={node.conclusion.length === 0 ? '...' : node.conclusion}
       />
-    );
+    ) : (
+        <input
+          type='text'
+          placeholder='...'
+          defaultValue={node.conclusion}
+          style={{ width: `${Math.max(3, node.conclusion.length)}ch` }}
+          onInput={(event) => {
+            let rootNodeCopy = { ...this.state.rootNode };
+
+            if (node.id === this.state.rootNode.id) {
+              rootNodeCopy.conclusion = event.target.value;
+            } else {
+              node.conclusion = event.target.value;
+            }
+
+            this.setState({
+              rootNode: rootNodeCopy
+            });
+          }}
+        />
+      );
 
     return (
       <div
         className='node-wrapper'
         key={node.id}
       >
-        <div className='label'>
+        <div
+          className='label'
+          onBlur={() => {
+            let rootNodeCopy = { ...this.state.rootNode };
+
+            if (node.id === this.state.rootNode.id) {
+              rootNodeCopy.labelFocused = false;
+            } else {
+              node.labelFocused = false;
+            }
+
+            this.setState({
+              rootNode: rootNodeCopy
+            });
+          }}
+          onClick={() => {
+            let rootNodeCopy = { ...this.state.rootNode };
+
+            if (node.id === this.state.rootNode.id) {
+              rootNodeCopy.labelFocused = true;
+            } else {
+              node.labelFocused = true;
+            }
+
+            this.setState({
+              rootNode: rootNodeCopy
+            });
+          }}
+        >
           {label}
         </div>
         <div className='node'>
@@ -137,6 +184,9 @@ class LiveEditor extends React.Component {
                 const newAssumption = {
                   parent: node,
                   id: uuidv4(),
+                  conclusionFocused: false,
+                  labelFocused: false,
+                  ruleNameFocused: false,
                   assumptions: [],
                   label: '',
                   ruleName: '',
@@ -183,11 +233,65 @@ class LiveEditor extends React.Component {
           </div>
           <div
             className='conclusion'
+            onBlur={() => {
+              let rootNodeCopy = { ...this.state.rootNode };
+
+              if (node.id === this.state.rootNode.id) {
+                rootNodeCopy.conclusionFocused = false;
+              } else {
+                node.conclusionFocused = false;
+              }
+
+              this.setState({
+                rootNode: rootNodeCopy
+              });
+            }}
+            onClick={() => {
+              let rootNodeCopy = { ...this.state.rootNode };
+
+              if (node.id === this.state.rootNode.id) {
+                rootNodeCopy.conclusionFocused = true;
+              } else {
+                node.conclusionFocused = true;
+              }
+
+              this.setState({
+                rootNode: rootNodeCopy
+              });
+            }}
           >
             {conclusion}
           </div>
         </div>
-        <div className='rule-name'>
+        <div
+          className='rule-name'
+          onBlur={() => {
+            let rootNodeCopy = { ...this.state.rootNode };
+
+            if (node.id === this.state.rootNode.id) {
+              rootNodeCopy.ruleNameFocused = false;
+            } else {
+              node.ruleNameFocused = false;
+            }
+
+            this.setState({
+              rootNode: rootNodeCopy
+            });
+          }}
+          onClick={() => {
+            let rootNodeCopy = { ...this.state.rootNode };
+
+            if (node.id === this.state.rootNode.id) {
+              rootNodeCopy.ruleNameFocused = true;
+            } else {
+              node.ruleNameFocused = true;
+            }
+
+            this.setState({
+              rootNode: rootNodeCopy
+            });
+          }}
+        >
           {ruleName}
         </div>
       </div >
@@ -256,169 +360,6 @@ class LiveEditor extends React.Component {
     )
   }
 }
-
-/*
-function Square(props) {
-  return (
-    <button
-      className="square"
-      onClick={props.onClick}
-    >
-      {props.value}
-    </button>
-  );
-}
-
-Square.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  value: PropTypes.number.isRequired
-}
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-Board.propTypes = {
-  squares: PropTypes.object.isRequired,
-  onClick: PropTypes.func.isRequired
-}
-
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      stepNumber: 0,
-      xIsNext: true
-    };
-  }
-
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[this.state.stepNumber];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
-    });
-  }
-
-  render() {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const winner = calculateWinner(current.squares);
-    
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
-
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-*/
 
 ReactDOM.render(<LiveEditor />,
   document.getElementById('root'));
